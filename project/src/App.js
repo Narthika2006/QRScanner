fcb3a9fdb20f360514027d03f512789c432907aa
Narthika2006 <narthikam@gmail.com>
2025-01-22 19:09:52 +0530

    commit

---
+++ project/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import Img from'./qrimg.jpeg';
+const App = () => {
+  // parameters
+  const[img,setimg]=useState(Img);
+  const[load,setload]=useState(false);
+  const[qrdata,setqrdata]=useState("");
+  const[size,setSize]=useState(150);
+
+  function Qr_gen()
+  {
+  try{
+       setload(true);
+       const url=`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(qrdata)}&size=${size}x${size}`;
+       setimg(url);
+  }
+  catch(error){
+    console.log("Error occurs while gendrating the Qr-Code")
+  }
+  finally{
+    setload(false);
+  }
+}
+  return (
+    <div>
+      <h1>QR CODE GENERATOR</h1>
+      {/* conditional renderring */}
+      {load && <p>please wait</p>}
+      <img src={img}  alt ='qr-box' className='QR-img'></img>
+
+      <label className='input-label'>Data for QR-Code</label>
+      <input type="text" id="data-input" placeholder='Enter data for Qr code' onChange={(e)=>setqrdata(e.target.value)}></input>
+      <label className='input-label'>
+        ImageSize(eg:150)
+      </label>
+      <input type="text" id="sizeinput" placeholder='Enter image size' onChange={(e)=>setSize(e.target.value)}></input>
+      <button className="gen-btn" onClick={Qr_gen} target="_blank">Gendrate QR-Code</button>
+      <button
+        className="download"
+        onClick={() => {
+          const link = document.createElement('a');
+          link.href = img;
+          link.download = 'code.png';
+          link.click();
+        }}
+      >
+        Download Your QR Code
+      </button>
+      
+    </div>
+  )
+}
+
+export default App
