196c33b32d852484aed29bcb33da006936765af1
Narthika2006 <narthikam@gmail.com>
2025-01-23 15:49:06 +0530

    commit

---
+++ project/build/precache-manifest.d2231a0ada941b545826f4d62786e268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc41a04a6864fd62f9eb8a80a871cf4",
+    "url": "/Scanner/index.html"
+  },
+  {
+    "revision": "45b47c6f15a35d6e68d0",
+    "url": "/Scanner/static/css/main.f5a08775.chunk.css"
+  },
+  {
+    "revision": "91532fe74c9ded204142",
+    "url": "/Scanner/static/js/2.a7f37d0f.chunk.js"
+  },
+  {
+    "revision": "45b47c6f15a35d6e68d0",
+    "url": "/Scanner/static/js/main.1b135bfb.chunk.js"
+  },
+  {
+    "revision": "d4af044fdb36b2298d48",
+    "url": "/Scanner/static/js/runtime~main.653a9e88.js"
+  },
+  {
+    "revision": "183ce40afcac987a08a56f91eaff1556",
+    "url": "/Scanner/static/media/qrimg.183ce40a.jpeg"
+  }
+]);
